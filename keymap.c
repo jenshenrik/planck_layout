a1e5e9cf9967a6413c2367b1fca5d90b83929c85
Jens Henrik Vogelius <jens.vogelius@isobar.com>
2020-11-24 10:40:22 +0100

    Revert "Added mod-tap to use shift keys as caps lock"
    This reverts commit f58c58d0caa599b4bd7ec2db71ef8cfbed24e79d.

--- keymap.c
+++ keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = {
   {KC_ESC   , KC_Q   , KC_W   , KC_E   , KC_R     , KC_T  , KC_Y  , KC_U     , KC_I   , KC_O   , KC_P     , MO(_SYST) },
   {KC_TAB   , KC_A   , KC_S   , KC_D   , KC_F     , KC_G  , KC_H  , KC_J     , KC_K   , KC_L   , MO(_MOVE), KC_ENT    },
-  {LSFT_T(KC_CAPS)  , KC_Z   , KC_X   , KC_C   , KC_V     , KC_B  , KC_N  , KC_M     , KC_COMM, KC_DOT , DK_SCLN  , RSFT_T(KC_CAPS)},
+  {KC_LSFT  , KC_Z   , KC_X   , KC_C   , KC_V     , KC_B  , KC_N  , KC_M     , KC_COMM, KC_DOT , DK_SCLN  , KC_RSFT   },
   {KC_LCTL, MO(_FUNC), KC_LALT, KC_LGUI, MO(_NUMB), KC_SPC, KC_SPC, MO(_SYMB), KC_RGUI, KC_RALT, MO(_FUNC)  , KC_RCTL }
 },
 
